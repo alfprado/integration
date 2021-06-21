69eabc9f9aeeb0fb8b68fd84c4c28c81b135e2aa
André Luiz Fernandes do Prado <prado_fernandes@yahoo.com.br>
2021-06-21 02:25:34 -0300

    adiciona router

--- main.py
+++ main.py
@@ -1,43 +1,9 @@
-from fastapi import FastAPI, Request
-from tasks import create_lead, create_person, update_person
-from celery import chain
-import json
+from fastapi import FastAPI
+from router import router
 
 app = FastAPI()
 
-@app.post("/create_lead")
-async def create_person_pipdrive(request: Request):
-    
-    json = await request.json()
+app.include_router(router)
 
-    nome = json['leads'][0]['name']
-    email = json['leads'][0]['email']
-    uuid = json['leads'][0]['uuid']
-    telefone = json['leads'][0]['personal_phone']
-
-    data = {'nome': nome, 'email': email, 'uuid': uuid, 'telefone': telefone}
-
-    task_id = chain(
-        create_person.s(data),
-        create_lead.s(),)()
-
-@app.post('/update_person')
-async def update_person_pipdrive(request: Request):
-
-    json = await request.json()
-    
-    email = json['leads'][0]['email']
-    uuid = json['leads'][0]['uuid']
-    profissao = json['leads'][0]['last_conversion']['content']['Profissão']
-    endereco = json['leads'][0]['last_conversion']['content']['Endereço']
-    documento = json['leads'][0]['last_conversion']['content']['Documento']
-    
-    data = {
-        'email': email, 
-        'uuid': uuid, 
-        'profissao': profissao,
-        'endereco': endereco, 
-        'documento': documento
-    }
-    
-    task_id = update_person.delay(data)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ router.py
@@ -0,0 +1,45 @@
+from fastapi import Request, APIRouter
+from tasks import create_lead, create_person, update_person
+from celery import chain
+import json
+
+router = APIRouter(
+    tags=['Pipdrive']
+)
+
+@router.post("/create_lead")
+async def create_person_pipdrive(request: Request):
+    
+    json = await request.json()
+
+    nome = json['leads'][0]['name']
+    email = json['leads'][0]['email']
+    uuid = json['leads'][0]['uuid']
+    telefone = json['leads'][0]['personal_phone']
+
+    data = {'nome': nome, 'email': email, 'uuid': uuid, 'telefone': telefone}
+
+    task_id = chain(
+        create_person.s(data),
+        create_lead.s(),)()
+
+@router.post('/update_person')
+async def update_person_pipdrive(request: Request):
+
+    json = await request.json()
+    
+    email = json['leads'][0]['email']
+    uuid = json['leads'][0]['uuid']
+    profissao = json['leads'][0]['last_conversion']['content']['Profissão']
+    endereco = json['leads'][0]['last_conversion']['content']['Endereço']
+    documento = json['leads'][0]['last_conversion']['content']['Documento']
+    
+    data = {
+        'email': email, 
+        'uuid': uuid, 
+        'profissao': profissao,
+        'endereco': endereco, 
+        'documento': documento
+    }
+    
+    task_id = update_person.delay(data)
